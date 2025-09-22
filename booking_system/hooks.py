2b607bb7fe0292b2f67b0245e429f65657cb36a5
keerthana <keerthanachandrank@Keerthanas-MacBook-Air.local>
2025-09-22 12:00:19 +0530

    feat: Add webform for booking

--- booking_system/hooks.py
+++ booking_system/hooks.py
@@ -246,4 +246,5 @@ fixtures = [
     {"dt": "Workflow State", "filters": [["name", "in", ["Pending", "Review", "Approved", "Rejected"]]]},
     {"dt": "Workflow", "filters": [["name", "in", ["Booking"]]]},
     {"dt": "Workflow Action Master", "filters": [["name", "in", ["Review", "Approve", "Reject"]]]},
+    {"dt": "Web Form", "filters": [["name","in",["Booking"]]]}
 ]
